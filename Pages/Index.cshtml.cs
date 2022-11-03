c0b9d09b32b6b2585f0ed44d8071b1dae7721a29
Axel <111660292+RSVTAxelrpg@users.noreply.github.com>
2022-11-03 14:58:28 -0600

    03/11/2022

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace Foro.Pages
 {
@@ -14,6 +15,29 @@ namespace Foro.Pages
 
         public void OnGet()
         {
+            login();
+        }
+
+        private void login()
+        {
+            using (MySqlConnection c = new MySqlConnection("Server=localhost;Database=foro;Uid=root;Password="))
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                c.Open();
+                cmd.Connection = c;
+                cmd.CommandText = "SELECT * FROM users WHERE username='admin' AND password='123'";
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        ViewData["Mensaje"] = "Si existe el usuario";
+                    } 
+                    else
+                    {
+                        ViewData["Mensaje"] = "Error, no se encontro al usuario";
+                    }
+                }
+            }
 
         }
     }
